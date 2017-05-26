e97af0a247a10a0f6348608a652c2acb0d7edc46
pmo-cloud01 <pmo-cloud01>
2017-05-26 11:49:03 +0800

    refact pollServer,pollPerf-multiprocess to one connect per thread

--- pollPerf-multiprocess.py
+++ pollPerf-multiprocess.py
@@ -6,21 +6,31 @@ from threading import Thread
 import time
 
 
-THREAD_COUNT = 300
-CONN_PER_THREAD = 1000
+THREAD_COUNT = 10000
+CONN_PER_THREAD = 100
 
+num_Exception = 0
 
 def worker(count):
+    global num_Exception
+    bExcep = False
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect(('10.21.1.88', 10001))
     while count > 0:
         try:
-            sock = socket(AF_INET, SOCK_STREAM)
-            sock.connect(('localhost', 10001))
+            count -= 1
             sock.send(b'1')
             resp = sock.recv(100)
-            sock.close()
-            count -= 1
+            #time.sleep(0.1)
         except Exception as e:
             print "Perf-multiprocess.worker Exception:", e, "\n"
+            bExcep = True
+
+    if bExcep:
+        num_Exception += 1
+        print "num_Exception:", num_Exception
+    
+    sock.close()
 
 
 while THREAD_COUNT > 0:
