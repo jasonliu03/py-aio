be0270b9701af9ae932a0d2cf3b71db4954ab267
pmo-cloud01 <pmo-cloud01>
2017-05-26 14:11:19 +0800

    adjust selectSever and test to one connect per thread

--- selectPerf-multiprocess.py
+++ selectPerf-multiprocess.py
@@ -5,37 +5,28 @@ from socket import *
 from threading import Thread
 import time
 
-n = 0
-LOOP_COUNT = 30
-
 THREAD_COUNT = 400
 CONN_PER_THREAD = 1000
 
-def monitor(count):
-    global n
-    i = 0
-    while True:
-        if i >= count:
-            break
-        time.sleep(1)
-        print(n, 'reqs/sec')
-        n = 0
-        i += 1
-#Thread(target=monitor,args=(LOOP_COUNT,)).start()
+num_Exception = 0
 
 def worker(count):
+    global num_Exception
+    bExcep = False
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect(('localhost', 10000))
     while count > 0:
         try:
-            sock = socket(AF_INET, SOCK_STREAM)
-            sock.connect(('localhost', 10000))
+            count -= 1
             sock.send(b'1')
             resp = sock.recv(100)
-            sock.close()
-            count -= 1
         except Exception as e:
             print "selectPerf-multiprocess.worker Exception:", e, "\n"
+    if bExcep:
+        num_Exception += 1
+        print "num_Exception:", num_Exception
+    sock.close()
 
-#代码非常简单，通过全局变量n来统计qps(req/sec 每秒请求数)
 
 while THREAD_COUNT > 0:
     Thread(target=worker,args=(CONN_PER_THREAD,)).start()
--- selectServer.py
+++ selectServer.py
@@ -3,46 +3,73 @@ import socket,select
 from threading import Thread
 import time
 
-server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-server.bind(('',10000))
-server.listen(5)
-inputs=[server]
-
 from fib import fib
 
-def fib_handler(client):
-    while True:
-        try:
-            req = client.recv(100)
-            if not req:
-                inputs.remove(client)
-                break
-        except Exception as e:
-            print "selectServer.fib_handler Exception:", e
-            inputs.remove(client)
-            break
-        else:
-            n = int(req)
-            result = fib(n)
-            resp = str(result).encode('ascii') + b'\n'
-            client.send(resp)
+server=socket.socket()
+host=""
+port=10000
+server.bind((host,port))
+server.listen(5)
+inputs = [server]
 
+num_users = 0
+num_CLIENT_QUIT = 0
+num_Exception = 0
+
+def fib_handler(client):
+    global inputs 
+    global num_users
+    global num_CLIENT_QUIT
+    global num_Exception
+#    while True:
+    try:
+	req = client.recv(100)
+	if not req:
+            #print "disconnected:", client.getpeername()
+            inputs.remove(client)
+	    client.close()
+            num_users -= 1
+            num_CLIENT_QUIT += 1
+	    #break
+        else:
+	    n = int(req)
+	    result = fib(n)
+	    resp = str(result).encode('utf8') + b'\n'
+	    client.send(resp)
+    except Exception as e:
+	print "Server.fib_handler Exception:", e
+        inputs.remove(client)
+	client.close()
+        num_users -= 1
+        num_Exception += 1
+        
 
 MONITOR_LOOP_COUNT = 100
 num = 0
 num_done = 0
 num_all = 0
+num_ERROR = 0
+
 def monitor(count):
     global num
     global num_done
     global num_all
+    global num_users
+    global num_ERROR
+    global num_CLIENT_QUIT
+    global num_Exception
     while count > 0:
         time.sleep(1)
+        print "--------------------------------"
         print('request:', num, 'reqs/sec')
         print('finished:', num_done, 'reqs/sec')
         num_all += num
         print('num_all:', num_all)
+        print "inputs.len:", len(inputs)
+        print('num_users:', num_users)
+        print('num_ERROR:', num_ERROR)
+        print('num_CLIENT_QUIT:', num_CLIENT_QUIT)
+        print('num_Exception:', num_Exception)
         num = 0
         num_done = 0
         count -= 1
@@ -51,16 +78,23 @@ Thread(target=monitor,args=(MONITOR_LOOP_COUNT,)).start()
 
 while 1:
     rs,ws,es=select.select(inputs,[],[],1)
-    for r in rs:
+    for r in rs: 
         if r is server:
             try:
                 clientsock,clientaddr=r.accept();
                 #print "Connected:", clientaddr
+	        num_users += 1
                 inputs.append(clientsock);
             except Exception as e:
                 print "selectServer Exception:", e, "\n"
         else:
             num += 1
-	    fib_handler(r)
+            fib_handler(r)
             num_done += 1
+    for e in es:    
+        print "e in es..."
+        inputs.remove(e)
+        e.close() 
+	num_ERROR += 1
+        num_users -= 1
 
